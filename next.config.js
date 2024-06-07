921cc36a1250ac37cc64dd496885545f6b21b59d
sujithmu <sujith.sopanam@hotmail.com>
2024-06-07 10:09:00 +0200

    next config updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
+    output: 'standalone',
     pageExtensions: ['ts', 'tsx'],
     generateBuildId: async () => {
         return `${Date.now() + Math.round(Math.random() * 2441139)}`;
