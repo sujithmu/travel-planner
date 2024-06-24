898e1132904288885fef3d2b0c578c4262112950
Sujith Unnikrishnan <sujith.sopanam@hotmail.com>
2024-06-24 17:32:34 +0200

    signout update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar: React.FC = () => {
                       className="inline-flex h-10 items-center justify-center rounded-md bg-zinc-900 px-8 text-sm font-medium text-zinc-50 shadow transition-colors hover:bg-zinc-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90 dark:focus-visible:ring-zinc-300"
                       onClick={(e) => {
                           e.preventDefault();
-                          signOut();
+                          signOut({ callbackUrl: process.env.NEXTAUTH_URL });
                           router.push('/sign-in')
                       }}
                   >
